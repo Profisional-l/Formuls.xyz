9ca1de4dd0b4e290c77115a36276ef9218844eb9
Profisional-l <vanjasavsjuk@gmail.com>
2023-11-25 14:01:14 +0300

    darkmode

---
+++ theme.js
@@ -0,0 +1,32 @@
+window.onload = function() {
+    const savedTheme = localStorage.getItem('theme');
+    const isSwitchOn = localStorage.getItem('theme1');
+    console.log("is", isSwitchOn);
+    console.log(savedTheme);
+    if (savedTheme == 'light') {
+      document.body.setAttribute('light', '');
+      let switchElement = document.getElementById('checkboxswitcher');
+      switchElement.checked = true;
+    } else {
+      document.body.removeAttribute('light');
+      switchElement.checked = false;
+    }
+  }
+const handleChange = (isChecked) => {
+    if (isChecked) {
+      localStorage.setItem('theme', 'light');
+      localStorage.setItem('theme1', true);
+      document.body.setAttribute('light', '');
+      //var switchElement = document.getElementById('checkboxswitcher');
+      localStorage.setItem('isON', 'true');
+    } else {
+      localStorage.removeItem('theme');
+      document.body.removeAttribute('light');
+      localStorage.setItem('theme1', false);
+      //var switchElement = document.getElementById('checkboxswitcher');
+    
+    }
+  }
+ 
+
+
