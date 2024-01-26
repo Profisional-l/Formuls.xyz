851417044d0fbc3e8ee4e780d2cbfeb5b7e00431
Profisional-l <vanjasavsjuk@gmail.com>
2024-01-26 18:13:53 +0300

    fr

---
+++ service-worker.js
@@ -0,0 +1,54 @@
+const cacheName = 'formuls-cache-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/images/formulslogo.png',
+        // Add other resources to cache
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Удаление старых кэшей при обновлении версии приложения
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(keys.map((key) => {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetchAndCache(event.request);
+    })
+  );
+});
+
+function fetchAndCache(request) {
+  return fetch(request).then((response) => {
+    // Проверка успешности запроса
+    if (!response || response.status !== 200 || response.type !== 'basic') {
+      return response;
+    }
+
+    const responseToCache = response.clone();
+
+    // Кэширование нового ресурса
+    caches.open(cacheName).then((cache) => {
+      cache.put(request, responseToCache);
+    });
+
+    return response;
+  });
+}
