ed3764ef6350435f0eca4b04233572566574908e
Profisional-l <vanjasavsjuk@gmail.com>
2024-01-27 21:50:05 +0300

    rfr

--- fillscreen.js
+++ fillscreen.js
@@ -1,30 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const fullscreenButton = document.getElementById('fullscreenButton');
+function toggleFullScreen() {
+    var elem = document.documentElement;
 
-    // Проверяем, сохранено ли состояние fullscreen в localStorage
-    const isFullscreen = localStorage.getItem('isFullscreen') === 'true';
-
-    // Если состояние fullscreen сохранено, применяем его
-    if (isFullscreen) {
-        document.documentElement.requestFullscreen();
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
     }
-
-    // Обработчик клика по кнопке
-    fullscreenButton.addEventListener('click', function () {
-        // Переключаем состояние fullscreen
-        if (!document.fullscreenElement) {
-            document.documentElement.requestFullscreen();
-            localStorage.setItem('isFullscreen', 'true');
-        } else {
-            document.exitFullscreen();
-            localStorage.setItem('isFullscreen', 'false');
-        }
-    });
-
-    // Обработчик события выхода из fullscreen
-    document.addEventListener('fullscreenchange', function () {
-        if (!document.fullscreenElement) {
-            localStorage.setItem('isFullscreen', 'false');
-        }
-    });
-});
+  }
