05d32d6a95c7563c405cb2ebc779d865f5c375d0
Profisional-l <vanjasavsjuk@gmail.com>
2024-02-11 21:34:02 +0300

    fff

--- service-worker.js
+++ service-worker.js
@@ -1,28 +1,12 @@
-const cacheName = 'formuls-cache-v1';
-
+// service-worker.js
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) => {
+    caches.open('my-cache').then((cache) => {
       return cache.addAll([
-        './',
+        '/',
         './index.html',
-        './manifest.json',
-        './images/formulslogo.png',
-        // Add other resources to cache
+        './css/styles.css'
       ]);
-    })
-  );
-});
-
-self.addEventListener('activate', (event) => {
-  // Удаление старых кэшей при обновлении версии приложения
-  event.waitUntil(
-    caches.keys().then((keys) => {
-      return Promise.all(keys.map((key) => {
-        if (key !== cacheName) {
-          return caches.delete(key);
-        }
-      }));
     })
   );
 });
@@ -30,28 +14,7 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetchAndCache(event.request);
+      return response || fetch(event.request);
     })
   );
 });
-
-function fetchAndCache(request) {
-  return caches.open(cacheName).then((cache) => {
-    return fetch(request).then((response) => {
-      // Проверка успешности запроса
-      if (!response || response.status !== 200 || response.type !== 'basic') {
-        return response;
-      }
-
-      const responseToCache = response.clone();
-
-      // Кэширование нового ресурса
-      return cache.put(request, responseToCache).then(() => {
-        return response;
-      });
-    }).catch((error) => {
-      console.error('Fetch failed:', error);
-      throw error; // Прокидываем ошибку дальше
-    });
-  });
-}
