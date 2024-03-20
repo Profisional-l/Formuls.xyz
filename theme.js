adc3dc833359f24300e3442eb60f86d13ca04fc9
Profisional-l <vanjasavsjuk@gmail.com>
2024-03-20 21:05:23 +0300

    DescriptionAndReloadin

--- theme.js
+++ theme.js
@@ -1,3 +1,9 @@
+window.addEventListener('pageshow', function(event) {
+  if (event.persisted) {
+    window.location.reload();
+  }
+});
+
 const handleChange = (isChecked) => {
   localStorage.setItem('theme', isChecked)
 
