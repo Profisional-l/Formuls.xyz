defe6d97185a1452e2bdbc619d9a475b2a749ea2
Profisional-l <vanjasavsjuk@gmail.com>
2024-04-08 11:10:43 +0300

    3п53п

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@ self.addEventListener('install', (event) => {
         '/',
         './index.html',
         './css/styles.css',
-        './images',
+        './images/',
         './flip.js',
         './theme.js',
         './appdownload.html',
@@ -46,11 +46,11 @@ self.addEventListener('install', (event) => {
   );
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
 
