5026d7e51e4fb2127b28685b7561e8cc26938516
Profisional-l <vanjasavsjuk@gmail.com>
2024-04-08 11:26:38 +0300

    46р654р

--- service-worker.js
+++ service-worker.js
@@ -5,11 +5,44 @@ self.addEventListener('install', (event) => {
       return cache.addAll([
         '/',
         './index.html',
-        './css/styles.css'
+        './css/styles.css',
+        './appdownload.html',
+        './formulsCALC.html',
+        './gamepage.html',
+        './podborgame.html',
+        './kinimatica.html',
+        './dinamica.html',
+        './zaksochrn.html',
+        './gasewater.html',
+        './mkt.html',
+        './termodinamica.html',
+        './elektrtok.html',
+        './magnit.html',
+        './kolebania.html',
+        './volni.html',
+        './allformuls.html',
+        './optica.html',
+        './atom.html',
+        './electrostat.html',
+        './aboutai.html',
+        './kinematicagame.html',
+        './dinamicagame.html',
+        './zaksochrngame.html',
+        './gasewatergame.html',
+        './mktgame.html',
+        './termodinamicagame.html',
+        './elektrtokgame.html',
+        './magnitgame.html',
+        './kolebaniagame.html',
+        './volnigame.html',
+        './opticagame.html',
+        './atomgame.html',
+        './electrostatgame.html',
       ]);
     })
   );
 });
+
 
 // self.addEventListener('fetch', (event) => {
 //   event.respondWith(
