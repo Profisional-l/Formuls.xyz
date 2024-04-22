b36914942abc66710a5965c53f081723ab207f8b
Profisional-l <vanjasavsjuk@gmail.com>
2024-04-22 14:04:37 +0300

    r3g5ghhd

--- theme.js
+++ theme.js
@@ -1,8 +1,8 @@
-window.addEventListener('pageshow', function (event) {
+window.addEventListener("pageshow", function (event) {
   if (event.persisted) {
-    window.location.reload()
+    window.location.reload();
   }
-})
+});
 
 // // Выбираем все элементы с классом 'FAKElogo'
 // var fakelogoList = document.querySelectorAll('.FAKElogo');
@@ -17,149 +17,149 @@ window.addEventListener('pageshow', function (event) {
 // -----------------------------------------------theme-------------------------------------------------
 
 const handleChange = (isChecked) => {
-  localStorage.setItem('theme', isChecked)
+  localStorage.setItem("theme", isChecked);
 
   if (isChecked) {
-    document.body.setAttribute('light', '')
-    if (document.location.pathname.includes('formulsCALC.html')) {
-      document.body.setAttribute('theme', 'light')
+    document.body.setAttribute("light", "");
+    if (document.location.pathname.includes("formulsCALC.html")) {
+      document.body.setAttribute("theme", "light");
     }
   } else {
-    document.body.removeAttribute('light')
-    if (document.location.pathname.includes('formulsCALC.html')) {
-      document.body.setAttribute('theme', 'dark')
+    document.body.removeAttribute("light");
+    if (document.location.pathname.includes("formulsCALC.html")) {
+      document.body.setAttribute("theme", "dark");
     }
   }
-}
+};
 
-document.addEventListener('DOMContentLoaded', function () {
-  const isLightTheme = localStorage.getItem('theme') === 'true'
-  const switchElement = document.getElementById('checkboxswitcher')
-  console.log('isLightTheme -', isLightTheme)
-  switchElement.checked = isLightTheme
+document.addEventListener("DOMContentLoaded", function () {
+  const isLightTheme = localStorage.getItem("theme") === "true";
+  const switchElement = document.getElementById("checkboxswitcher");
+  console.log("isLightTheme -", isLightTheme);
+  switchElement.checked = isLightTheme;
 
   if (isLightTheme) {
-    document.body.setAttribute('light', '')
+    document.body.setAttribute("light", "");
   } else {
-    document.body.removeAttribute('light')
+    document.body.removeAttribute("light");
   }
 
   setTimeout(function () {
     if (isLightTheme) {
-      switchElement.checked = true
+      switchElement.checked = true;
     } else {
-      switchElement.checked = false
+      switchElement.checked = false;
     }
-  }, 100)
+  }, 100);
 
   // -----------------------------------------------^^theme^^-------------------------------------------------
 
-  var userAgent = navigator.userAgent.toLowerCase()
-  var isAndroid = userAgent.indexOf('formulsandroiduseragent') !== -1
+  var userAgent = navigator.userAgent.toLowerCase();
+  var isAndroid = userAgent.indexOf("formulsandroiduseragent") !== -1;
 
   // var isAndroid = true
 
   if (
     isAndroid ||
-    window.matchMedia('(display-mode: standalone)').matches ||
+    window.matchMedia("(display-mode: standalone)").matches ||
     window.navigator.standalone === true
   ) {
-    var godownloadElement = document.querySelector('.godownload')
-    var buttoappfooter = document.querySelector('.buttoappfooter')
-    var tocalcbut = document.querySelector('.tocalcbut')
-    let LogoName = document.querySelector('.logo')
-    let LogoIMG = document.querySelector('.logoIMG')
-    var linktoHOWwork = document.querySelector('.linkFromMain')
-    let mainscreenh2 = document.querySelector('.mainscreenh2')
-    let mainscreenhiH3 = document.querySelector('.mainscreenhi h3')
-    let categoriesfield = document.getElementById('categoriesfield')
-    let footer = document.querySelector('footer')
-    let maintextHI = document.querySelectorAll('.textonmain')
-    let selectIT = document.getElementById('selectIT')
-    let switcherTheme = document.querySelector('.switch')
+    var godownloadElement = document.querySelector(".godownload");
+    var buttoappfooter = document.querySelector(".buttoappfooter");
+    var tocalcbut = document.querySelector(".tocalcbut");
+    let LogoName = document.querySelector(".logo");
+    let LogoIMG = document.querySelector(".logoIMG");
+    var linktoHOWwork = document.querySelector(".linkFromMain");
+    let mainscreenh2 = document.querySelector(".mainscreenh2");
+    let mainscreenhiH3 = document.querySelector(".mainscreenhi h3");
+    let categoriesfield = document.getElementById("categoriesfield");
+    let footer = document.querySelector("footer");
+    let maintextHI = document.querySelectorAll(".textonmain");
+    let selectIT = document.getElementById("selectIT");
+    let switcherTheme = document.querySelector(".switch");
 
     if (
-      document.location.pathname === 'https://formuls.xyz/' ||
-      document.location.pathname === '/' ||
-      document.location.pathname === 'https://formuls.xyz'
+      document.location.pathname === "https://formuls.xyz/" ||
+      document.location.pathname === "/" ||
+      document.location.pathname === "https://formuls.xyz"
     ) {
-      switcherTheme.style.marginTop = '55px'
+      switcherTheme.style.marginTop = "55px";
     } else {
-      switcherTheme.style.display = 'none'
+      switcherTheme.style.display = "none";
     }
 
     if (
       mainscreenh2 &&
-      !document.location.pathname.includes('allformuls.html') &&
-      !document.location.pathname.includes('aboutai.html')
+      !document.location.pathname.includes("allformuls.html") &&
+      !document.location.pathname.includes("aboutai.html")
     ) {
-      mainscreenh2.style.display = 'none'
+      mainscreenh2.style.display = "none";
       if (mainscreenhiH3) {
-        mainscreenhiH3.style.display = 'none'
+        mainscreenhiH3.style.display = "none";
       }
-      footer.style.display = 'none'
+      footer.style.display = "none";
       maintextHI.forEach(function (e) {
-        e.style.display = 'none'
-      })
+        e.style.display = "none";
+      });
     }
     if (categoriesfield) {
-      categoriesfield.style.display = 'none'
-      selectIT.style.display = 'none'
+      categoriesfield.style.display = "none";
+      selectIT.style.display = "none";
     }
 
     if (linktoHOWwork) {
-      linktoHOWwork.style.display = 'none'
+      linktoHOWwork.style.display = "none";
     }
     function changeLogoType() {
-      LogoName.classList.add('changeLOGOout')
-      LogoIMG.classList.add('changeLOGOin')
+      LogoName.classList.add("changeLOGOout");
+      LogoIMG.classList.add("changeLOGOin");
     }
-    changeLogoType()
+    changeLogoType();
     if (tocalcbut) {
-      tocalcbut.style.right = '20px'
+      tocalcbut.style.right = "20px";
     }
     if (godownloadElement) {
-      godownloadElement.style.display = 'none'
+      godownloadElement.style.display = "none";
       if (buttoappfooter) {
-        buttoappfooter.style.display = 'none'
+        buttoappfooter.style.display = "none";
       }
     }
   }
 
   if (
     !isAndroid &&
-    !window.matchMedia('(display-mode: standalone)').matches &&
+    !window.matchMedia("(display-mode: standalone)").matches &&
     !window.navigator.standalone === true
   ) {
-    var fakeheaders = document.querySelectorAll('.FakeHeader')
-    var fakeTocalcs = document.querySelectorAll('.FakeCalc')
+    var fakeheaders = document.querySelectorAll(".FakeHeader");
+    var fakeTocalcs = document.querySelectorAll(".FakeCalc");
     fakeheaders.forEach(function (element) {
-      element.style.display = 'none';
-    })
+      element.style.display = "none";
+    });
     fakeTocalcs.forEach(function (element) {
-      element.style.display = 'none';
-    })
+      element.style.display = "none";
+    });
   }
 
-  let LogoName = document.querySelector('.logo')
-  let LogoIMG = document.querySelector('.logoIMG')
-  let scrolled = false
-  let scrollToTopBtn = document.getElementById('scrollToTopBtn')
+  let LogoName = document.querySelector(".logo");
+  let LogoIMG = document.querySelector(".logoIMG");
+  let scrolled = false;
+  let scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
   if (
-    !document.location.pathname.includes('formulsCALC.html') &&
-    !document.location.pathname.includes('aboutai.html')
+    !document.location.pathname.includes("formulsCALC.html") &&
+    !document.location.pathname.includes("aboutai.html")
   ) {
     window.onscroll = function () {
-      scrollFunction()
+      scrollFunction();
       if (
         !isAndroid &&
-        !window.matchMedia('(display-mode: standalone)').matches &&
+        !window.matchMedia("(display-mode: standalone)").matches &&
         !window.navigator.standalone === true
       ) {
-        scrollLOGO()
+        scrollLOGO();
       }
-    }
+    };
   }
 
   function scrollLOGO() {
@@ -168,26 +168,26 @@ document.addEventListener('DOMContentLoaded', function () {
       document.documentElement.scrollTop >= 70
     ) {
       if (!scrolled) {
-        LogoName.classList.remove('changeLOGOin')
-        LogoIMG.classList.remove('changeLOGOout')
-        LogoName.classList.add('changeLOGOout')
-        LogoIMG.classList.add('changeLOGOin')
-        scrolled = true
+        LogoName.classList.remove("changeLOGOin");
+        LogoIMG.classList.remove("changeLOGOout");
+        LogoName.classList.add("changeLOGOout");
+        LogoIMG.classList.add("changeLOGOin");
+        scrolled = true;
       }
     } else {
       if (scrolled) {
-        LogoName.classList.remove('changeLOGOout')
-        LogoIMG.classList.remove('changeLOGOin')
-        LogoName.classList.add('changeLOGOin')
-        LogoIMG.classList.add('changeLOGOout')
-        scrolled = false
+        LogoName.classList.remove("changeLOGOout");
+        LogoIMG.classList.remove("changeLOGOin");
+        LogoName.classList.add("changeLOGOin");
+        LogoIMG.classList.add("changeLOGOout");
+        scrolled = false;
       }
     }
   }
 
-  if (document.location.pathname.includes('game')) {
-    LogoName.classList.add('changeLOGOout')
-    LogoIMG.classList.add('changeLOGOin')
+  if (document.location.pathname.includes("game")) {
+    LogoName.classList.add("changeLOGOout");
+    LogoIMG.classList.add("changeLOGOin");
   }
 
   if (scrollToTopBtn) {
@@ -196,87 +196,87 @@ document.addEventListener('DOMContentLoaded', function () {
         document.body.scrollTop > 150 ||
         document.documentElement.scrollTop > 150
       ) {
-        scrollToTopBtn.style.display = 'block'
+        scrollToTopBtn.style.display = "block";
       } else {
-        scrollToTopBtn.style.display = 'none'
+        scrollToTopBtn.style.display = "none";
       }
     }
-    scrollToTopBtn.addEventListener('click', function () {
-      scrollToTop()
-    })
+    scrollToTopBtn.addEventListener("click", function () {
+      scrollToTop();
+    });
   }
 
   function scrollToTop() {
     var currentScroll =
-      document.documentElement.scrollTop || document.body.scrollTop
+      document.documentElement.scrollTop || document.body.scrollTop;
 
     if (currentScroll > 0) {
-      window.requestAnimationFrame(scrollToTop)
-      window.scrollTo(0, currentScroll - currentScroll / 5)
+      window.requestAnimationFrame(scrollToTop);
+      window.scrollTo(0, currentScroll - currentScroll / 5);
     }
   }
-})
+});
 
 document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: 'smooth',
-    })
-  })
-})
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
 
-let maska = document.querySelector('.mask')
-window.addEventListener('load', () => {
-  maska.classList.add('hide')
+let maska = document.querySelector(".mask");
+window.addEventListener("load", () => {
+  maska.classList.add("hide");
   setTimeout(() => {
-    maska.remove()
-  }, 200)
-})
+    maska.remove();
+  }, 200);
+});
 // maska.style.display = 'none'
 $(document).ready(function () {
   // Используем событие pageshow
-  $(window).on('pageshow', function (event) {
+  $(window).on("pageshow", function (event) {
     if (event.originalEvent.persisted) {
       // Скрываем overlay при возврате на страницу из истории
-      $('.page-transition-overlay').css('display', 'none')
+      $(".page-transition-overlay").css("display", "none");
     }
-  })
+  });
 
   $(document).on(
-    'click',
+    "click",
     'a:not([href^="#maintext"]):not([href^="https://t.me/anttany"]):not([href^="https://t.me/jakesooly"]):not([href^="https://is.gd/qPembH"]):not([href^="https://play.google.com/store/apps/details?id=com.finetest.formuls"]), .gobackbut, .buttogame',
     function (e) {
-      e.preventDefault()
-      var href = $(this).attr('href')
+      e.preventDefault();
+      var href = $(this).attr("href");
 
       // Показываем overlay
-      $('.page-transition-overlay').fadeIn(260, function () {
+      $(".page-transition-overlay").fadeIn(260, function () {
         // После показа overlay, переходим на новую страницу
-        window.location.href = href
-      })
-    },
-  )
-})
+        window.location.href = href;
+      });
+    }
+  );
+});
 
 document
   .querySelector("meta[name='apple-mobile-web-app-status-bar-style']")
-  .setAttribute('content', '#252525')
+  .setAttribute("content", "#252525");
 function toggleFullScreen() {
-  var elem = document.documentElement
+  var elem = document.documentElement;
 
   if (elem.requestFullscreen) {
-    elem.requestFullscreen()
+    elem.requestFullscreen();
   } else if (elem.mozRequestFullScreen) {
     /* Firefox */
-    elem.mozRequestFullScreen()
+    elem.mozRequestFullScreen();
   } else if (elem.webkitRequestFullscreen) {
     /* Chrome, Safari and Opera */
-    elem.webkitRequestFullscreen()
+    elem.webkitRequestFullscreen();
   } else if (elem.msRequestFullscreen) {
     /* IE/Edge */
-    elem.msRequestFullscreen()
+    elem.msRequestFullscreen();
   }
 }
 
@@ -284,14 +284,14 @@ function toggleFullScreen() {
 // const beforeElement = window.getComputedStyle(PoMswitch, '::before');
 // PoMswitch.style.setProperty('--before-color', 'red');
 // console.log(PoMswitch)
-if (document.location.pathname.includes('mathindex.html')) {
+if (document.location.pathname.includes("mathindex.html")) {
   document.documentElement.style.setProperty(
-    '--var-MoP-img',
-    'url("../images/mathlogo.png")',
-  )
-  var PoMswith = document.querySelector('.PoM')
-  PoMswith.classList.add('ifmathpage')
-  console.log(PoMswith.classList)
+    "--var-MoP-img",
+    'url("../images/mathlogo.png")'
+  );
+  var PoMswith = document.querySelector(".PoM");
+  PoMswith.classList.add("ifmathpage");
+  console.log(PoMswith.classList);
 }
 
 // if (document.location.pathname.includes('index.html')) {
